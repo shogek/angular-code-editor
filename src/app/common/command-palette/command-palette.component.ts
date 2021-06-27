a80358df458da82b4d0cc39b550378efc460295c
Benas Bagdanovas <benas.bagdanovas@gmail.com>
2021-06-27 16:59:51 +0300

    #18 Save progress - moves down on arrow keys

--- src/app/common/command-palette/command-palette.component.ts
+++ src/app/common/command-palette/command-palette.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, Input, OnInit } from "@angular/core";
+import { ChangeDetectionStrategy, Component, HostListener, Input, OnInit } from "@angular/core";
 import { CommandPaletteItem } from "./command-palette-item";
 
 @Component({
@@ -18,5 +18,29 @@ export class CommandPaletteComponent implements OnInit {
         const documentMiddle = document.body.clientWidth / 2;
         const menuStart = documentMiddle - (this.MENU_WIDTH_PX / 2);
         this.leftX = menuStart;
+    }
+
+    @HostListener('document:keydown', ['$event'])
+    private onKeyDown(e: KeyboardEvent) {
+        const { key } = e;
+        if (key !== 'KeyDown' && key !== 'KeyUp') {
+            return;
+        }
+
+        // TODO: Are these needed?
+        e.preventDefault();
+        e.stopPropagation();
+
+        let nextIndex = 0;
+        const activeIndex = this.choices.findIndex(choice => choice.isActive);
+        if (activeIndex < this.choices.length - 1) {
+            nextIndex = activeIndex + 1;
+        }
+
+        const activeChoice = this.choices[activeIndex];
+        activeChoice.isActive = false;
+
+        const nextChoice = this.choices[nextIndex];
+        nextChoice.isActive = true;
     }
 }
